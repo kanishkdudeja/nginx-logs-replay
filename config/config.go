55dc945a6cba703a5a185499e65dcdd27329c640
Manish <manish.gill@wingify.com>
2018-10-08 02:46:02 +0530

    Removed unnecessary string-to-bool if-else checks for boolean flags. Use BoolVar directly

--- config/config.go
+++ config/config.go
@@ -23,15 +23,15 @@ type Config struct {
 // InitializeConfig Returns the command line flags
 // and calls the config package to load up the configuration
 func InitializeConfig() (*Config, error) {
-	var dryRun string
+	var dryRun bool
 	var baseURL string
 	var logFilePath string
-	var includeTimeStamp string
+	var includeTimeStamp bool
 
-	flag.StringVar(&dryRun, "dry-run", "uninitialized", "Denotes whether it's a dry run or not")
+	flag.BoolVar(&dryRun, "dry-run", false, "Denotes whether it's a dry run or not")
 	flag.StringVar(&baseURL, "base-url", "uninitialized", "Denotes the host name to which requests will be replayed. Eg: https://website.com / 1.1.1.1")
 	flag.StringVar(&logFilePath, "log-file-path", "uninitialized", "Denotes the path at which the log file is present. Eg: /var/log/nginx/access.log")
-	flag.StringVar(&includeTimeStamp, "include-timestamp", "uninitialized", "Denotes whether we need to send the UNIX timestamp along with the URL")
+	flag.BoolVar(&includeTimeStamp, "include-timestamp", true, "Denotes whether we need to send the UNIX timestamp along with the URL")
 
 	flag.Parse()
 
@@ -43,34 +43,12 @@ func InitializeConfig() (*Config, error) {
 		return nil, errors.New("Please supply the path of the log file as a parameter. Eg: ./replay --log-file-path=/var/log/nginx/access.log")
 	}
 
-	if dryRun == "uninitialized" {
-		return nil, errors.New("Please supply the dry-run parameter. Pass as 'true' if you want the script to only print the URLs. Eg: ./replay --dry-run=true/false")
-	}
-
-	if dryRun != "true" && dryRun != "false" {
-		return nil, errors.New("The dry-run parameter can only have a value of true/false. Eg: ./replay --dry-run=true/false")
-	}
-
-	if includeTimeStamp != "uninitialized" && includeTimeStamp != "true" && includeTimeStamp != "false" {
-		return nil, errors.New("The include-timestamp parameter can only have a value of true/false. Eg: ./replay --include-timestamp=true/false")
-	}
-
 	var configObj Config
 
-	if dryRun == "true" {
-		configObj.DryRun = true
-	} else {
-		configObj.DryRun = false
-	}
-
+	configObj.DryRun = dryRun
 	configObj.BaseURL = baseURL
 	configObj.LogFilePath = logFilePath
-
-	if includeTimeStamp == "true" {
-		configObj.IncludeTimeStamp = true
-	} else {
-		configObj.IncludeTimeStamp = false
-	}
+	configObj.IncludeTimeStamp = includeTimeStamp
 
 	return &configObj, nil
 }
